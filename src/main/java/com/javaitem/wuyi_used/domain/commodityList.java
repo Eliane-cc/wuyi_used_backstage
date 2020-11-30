4a31bf546101d5baa9b9ad16477ec8bcf31de98a
Elaine <1807332757@qq.com>
2020-11-30 15:03:01 +0800

    feat: 获取商品列表接口

--- src/main/java/com/javaitem/wuyi_used/domain/commodityList.java
+++ src/main/java/com/javaitem/wuyi_used/domain/commodityList.java
@@ -25,6 +25,17 @@ public class commodityList implements Serializable {
     /*是否购买*/
     private Integer isSold;
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /*是否购买*/
+    private String userName;
+
     public Integer getCommodityId() {
         return commodityId;
     }
